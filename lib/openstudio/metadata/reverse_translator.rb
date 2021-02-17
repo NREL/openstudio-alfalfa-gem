470170d15e7dc6b1ccd00d7fa275a971e869859b
TShapinsky <tobiasshapinsky@gmail.com>
2021-02-17 12:03:42 -0700

    reverse translator spec checks and reverse translator gives equip and loops as output

--- lib/openstudio/metadata/reverse_translator.rb
+++ lib/openstudio/metadata/reverse_translator.rb
@@ -6,42 +6,38 @@ module OpenStudio
   module Metadata
     class ReverseTranslator
       IGNORE_TAGS = ['sensor', 'meter']
-      def initialize(path_to_model)
+      attr_reader :equips, :loops
+      def initialize(path_to_model, path_to_mappings_dir = nil)
         @model = JSON.parse(File.read(path_to_model))
-        @mappings_manager = OpenStudio::Metadata::Mapping::MappingsManager.new
+        if path_to_mappings_dir.nil?
+          @mappings_manager = OpenStudio::Metadata::Mapping::MappingsManager.new
+        else
+          @mappings_manager = OpenStudio::Metadata::Mapping::MappingsManager.new(path_to_mappings_dir)
+        end
         @mappings = @mappings_manager.mappings
         @class_map = {}
         @mappings.each do |mapping|
           @class_map[mapping.openstudio_class] = @mappings_manager.template_manager.resolve_metadata(mapping.template_ids[HAYSTACK])
         end
+        @equips = []
+        @loops = []
       end
 
       def reverse_translate
-        equips = []
-        File.open('log.txt', 'w') do |f|
-          @model['rows'].each do |row|
-            next unless (IGNORE_TAGS & row.keys).empty?
-            openstudio_class = find_matching_class(row)
-            equips.push(OpenStudio::Metadata::Topology::Equipment.new(openstudio_class, row))
-            f.write openstudio_class
-            f.write("\n")
-            f.write row
-            f.write("\n\n")
-          end
-          loop_builder = OpenStudio::Metadata::Topology::LoopBuilder.new(equips)
-          loops = loop_builder.build_loops
-          loops.each do |loop|
-            f.write("#{loop}\n")
-          end
+        @equips = []
+        @model['rows'].each do |row|
+          next unless (IGNORE_TAGS & row.keys).empty?
+          openstudio_class = find_matching_class(row)
+          @equips.push(OpenStudio::Metadata::Topology::Equipment.new(openstudio_class, row))
         end
+        loop_builder = OpenStudio::Metadata::Topology::LoopBuilder.new(equips)
+        @loops = loop_builder.build_loops
       end
 
       def find_matching_class(entry)
         score = Hash[*@class_map.keys.collect { |clazz| [clazz, 0] }.flatten]
-        # puts entry
         entry.each do |key, val|
           @class_map.each do |class_name, template|
-            # puts template
             if template.key? key
               score[class_name] = score[class_name] + 1
               if template[key] == val
--- spec/tests/reverse_translator_spec.rb
+++ spec/tests/reverse_translator_spec.rb
@@ -4,13 +4,17 @@ require_relative '../spec_helper'
 RSpec.describe 'Openstudio::Metadata::ReverseTranslator spec' do
   before(:all) do
     building_type = 'SmallOffice'
-    generate_building_metadata_and_write(building_type)
-    @dir = "#{Dir.pwd}/spec/outputs"
-    @model = File.join(@dir, "#{building_type}_model.json")
-    @reverse_translator = OpenStudio::Metadata::ReverseTranslator.new(@model)
+    @model = File.join(inputs_dir, "#{building_type}_model.json")
+    @reverse_translator = OpenStudio::Metadata::ReverseTranslator.new(@model, inputs_dir)
+    @reverse_translator.reverse_translate
   end
 
-  it 'Should do something' do
-    @reverse_translator.reverse_translate
+  it 'Should have 6 loops' do
+    expect(@reverse_translator.loops.size).to be 6
+  end
+
+  it 'Should have 6 Thermal Zones' do
+    zone_count = @reverse_translator.equips.count { |equip| equip.openstudio_class == 'OS:ThermalZone' }
+    expect(zone_count).to be 6
   end
 end
